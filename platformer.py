12ca16140c16ca07d7d816b31f28e2693ea40d9d
Marie Balle-Petersen <mariemihoko@gmail.com>
2023-06-26 10:54:31 +0200

    Added player

--- platformer.py
+++ platformer.py
@@ -7,6 +7,8 @@ SCREEN_WITH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
 
+CHARACTER_SCALING = 2
+
 class MyGame(arcade.Window):
     def __init__(self):
 
@@ -15,11 +17,21 @@ class MyGame(arcade.Window):
 
         arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
 
+        self.player_list = None
+        self.wall_list = None
+
     def setup(self):
         """
         Setup game. Call for reset
         """
-        pass
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
+        # Setup player sprite
+        self.player_list = arcade.SpriteList()
+        self.player_sprite = arcade.Sprite("images/tile_0019.png", CHARACTER_SCALING)
+        self.player_sprite.center_x = SCREEN_WITH/2
+        self.player_sprite.center_y = SCREEN_HEIGHT/2
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         """
@@ -27,7 +39,7 @@ class MyGame(arcade.Window):
         """
 
         self.clear()
-        # code to draw the screen goes here
+        self.player_list.draw()
 
 def main():
     """
