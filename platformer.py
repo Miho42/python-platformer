7b6ec0b83d8ec0b41c9f9ef0da865e0ae2699f85
Marie Mihoko Balle-Petersen <108263819+Miho42@users.noreply.github.com>
2023-06-26 11:57:01 +0200

    Player moves left and right (#6)

--- platformer.py
+++ platformer.py
@@ -12,6 +12,8 @@ TILE_SCALING = 2
 
 TILE_WIDTH = 42
 
+PLAYER_MOVEMENT_SPEED = 5
+
 class MyGame(arcade.Window):
     def __init__(self):
 
@@ -23,12 +25,17 @@ class MyGame(arcade.Window):
         # Scene object
         self.scene = None
 
+        # Variable for player sprite
+        self.player_sprite = None
+
+        # Physics engine
+        self.physics_engine = None
+
     def setup(self):
         """
         Setup game. Call for reset
         """
 
-<
         # Initialize scene
         self.scene = arcade.Scene()
 
@@ -37,7 +44,7 @@ class MyGame(arcade.Window):
         self.scene.add_sprite_list("Walls")
 
         # Create ground
-        for x in range(21, SCREEN_WITH, 42):
+        for x in range(21, SCREEN_WIDTH, 42):
 
             wall = arcade.Sprite("images/tile_0001.png", TILE_SCALING)
             wall.center_x = x
@@ -58,6 +65,12 @@ class MyGame(arcade.Window):
         self.player_sprite.center_x = SCREEN_WIDTH/2
         self.player_sprite.center_y = SCREEN_HEIGHT/2
         self.scene.add_sprite("Player", self.player_sprite)
+
+        # Create physics egnine
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.scene.get_sprite_list("Walls")
+        )
+
     def on_draw(self):
         """
         Render the screen
@@ -65,7 +78,35 @@ class MyGame(arcade.Window):
 
         self.clear()
         self.scene.draw()
+
+    def on_update(self, delta_time):
+        """
+        Movement and game logic
+        """
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+    def on_key_press(self, key, modifiers):
+        """
+        Called whenever a key is pressed
+        """
+
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
         
+    def on_key_release(self, key, modifiers):
+        """
+        Called whenever a key is released
+        """
+
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
 def main():
     """
     Main function
